a42e685e5540dbd501526852be61faa3f2faa131
Hudson <hudsonhdj36@gmail.com>
2019-04-15 21:33:38 -0300

    Adicionado métodos PUT e DELETE na interface de ServiceCliente.

--- app/src/main/java/com/eaj/ufrn/mobilemilk/ServiceInterface/ServiceCliente.java
+++ app/src/main/java/com/eaj/ufrn/mobilemilk/ServiceInterface/ServiceCliente.java
@@ -6,12 +6,21 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface ServiceCliente {
 
     @POST("/cliente")
     Call<Cliente> cadastrarCliente(@Body Cliente usuario);
 
+    @PUT("/cliente/{id}")
+    Call<Cliente> atualizarCliete(@Path("id") int id);
+
+    @DELETE("/cliente/{id}")
+    Call<Cliente> deletarCliente(@Path("id") int id);
+    
 }
