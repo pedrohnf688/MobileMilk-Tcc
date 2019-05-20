68d00e0bf18fe8cd44500c650d4d58c892d69f87
Hudson <hudsonhdj36@gmail.com>
2019-05-19 21:54:21 -0300

    BottomNavigation layout atualizado.

--- app/src/main/java/com/eaj/ufrn/mobilemilk/Activity/HomeActivity.java
+++ app/src/main/java/com/eaj/ufrn/mobilemilk/Activity/HomeActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
