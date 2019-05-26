8d84deb00dfd16e17b8f6de90e475d0f827b26b8
Hudson <hudsonhdj36@gmail.com>
2019-05-26 18:00:50 -0300

    Atualização de Enum de Produtos.

--- app/src/main/java/com/eaj/ufrn/mobilemilk/Enum/Produtos.java
+++ app/src/main/java/com/eaj/ufrn/mobilemilk/Enum/Produtos.java
@@ -1,5 +1,5 @@
 package com.eaj.ufrn.mobilemilk.Enum;
 
 public enum Produtos {
-    CREME30GORDURA, SORO, CREME45GORDURA;
+    CREME30GORDURA, SORO, CREME45GORDURA, BEBIDA_LACTEA, IORGUTE, SORVETE, QUEIJO;
 }
