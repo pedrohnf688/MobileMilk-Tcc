a8726350f1fe542cda59646fe079160d3bd45b38
Hudson <hudsonhdj36@gmail.com>
2019-05-28 20:53:55 -0300

    Atualização de Layout de cadastro de análises - Concluído.

--- app/src/main/java/com/eaj/ufrn/mobilemilk/Activity/CadastrarAnaliseActivity.java
+++ app/src/main/java/com/eaj/ufrn/mobilemilk/Activity/CadastrarAnaliseActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.eaj.ufrn.mobilemilk.Enum.Leite;
 import com.eaj.ufrn.mobilemilk.Enum.OrigemLeite;
@@ -36,9 +39,15 @@ public class CadastrarAnaliseActivity extends AppCompatActivity {
     private CheckBox analisePontoCrioscopico;
     private CheckBox analiseAcidezDornic;
     private CheckBox analiseDensidadeRelativa;
+    private CheckBox analiseContagemBacteriaTotal;
 
     // EditView numero de amostras
     private EditText numAmostras;
+
+    private TextView selecionarFraude;
+
+    // Layout com as análises de frauda
+    private LinearLayout contentFrauda;
 
 
     @Override
@@ -57,14 +66,19 @@ public class CadastrarAnaliseActivity extends AppCompatActivity {
         this.analisesCaseina = findViewById(R.id.caseina);
         this.analisesCelulasSomaticas = findViewById(R.id.celulasSomaticas);
         this.analisesNitrogenioUreico  = findViewById(R.id.nitroGenioUreico);
-        this.analisesCondutividadeEletrica = findViewById(R.id.celulasSomaticas);
+        this.analisesCondutividadeEletrica = findViewById(R.id.condutividadeeletrica);
         this.analisesResiduoAntibiotico = findViewById(R.id.residuoAntibiotico);
         this.analisesPh = findViewById(R.id.ph);
         this.analisePontoCrioscopico = findViewById(R.id.pontoCrioscopico);
         this.analiseAcidezDornic = findViewById(R.id.acidezDornic);
         this.analiseDensidadeRelativa = findViewById(R.id.densidadeRelativa);
+        this.analiseContagemBacteriaTotal = findViewById(R.id.contagemBacteriasTotais);
 
         this.numAmostras = findViewById(R.id.numAmostras);
+
+        this.selecionarFraude = findViewById(R.id.selecionarFraude);
+
+        this.contentFrauda = findViewById(R.id.contentFraude);
 
         // Adapter array para spinner de Origem de Leite
         ArrayAdapter<CharSequence> array = ArrayAdapter.createFromResource(getApplicationContext(), R.array.spinnerOrigemLeite,
@@ -84,7 +98,186 @@ public class CadastrarAnaliseActivity extends AppCompatActivity {
         array2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         this.produto.setAdapter(array2);
 
+        this.iniciaLayoutAnalise(); // Ajusta o Layout inicia de opções de análise
 
+        // Implementação de Listener para o spinner de produto
+        this.produto.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("ItemSelected", "Position: " + position + " id: " + id);
+
+                if(position != 7 && position != 6){
+
+                    /* Setar como invisível os spinners de origem e tipo de leite -> no caso quando
+                    *  o produto selecionado não for o leite.
+                    * */
+                    origemLeite.setVisibility(View.GONE);
+                    tipoLeite.setVisibility(View.GONE);
+                    selecionarFraude.setVisibility(View.GONE);
+                    contentFrauda.setVisibility(View.GONE);
+
+                    // Desabilita algumas análises
+                    analisesCelulasSomaticas.setVisibility(View.GONE);
+                    analisesCaseina.setVisibility(View.GONE);
+                    analisesNitrogenioUreico.setVisibility(View.GONE);
+                    analisePontoCrioscopico.setVisibility(View.GONE);
+                    analiseAcidezDornic.setVisibility(View.GONE);
+                    analisesResiduoAntibiotico.setVisibility(View.GONE);
+                    analiseDensidadeRelativa.setVisibility(View.GONE);
+
+                    // Habilita algumas análises
+                    analisesComposicao.setVisibility(View.VISIBLE);
+                    analisesPh.setVisibility(View.VISIBLE);
+                    analisesCondutividadeEletrica.setVisibility(View.VISIBLE);
+                    analiseContagemBacteriaTotal.setVisibility(View.VISIBLE);
+
+                }
+                else if(position == 6){
+
+                    /* Setar como invisível os spinners de origem e tipo de leite -> no caso quando
+                     *  o produto selecionado não for o leite.
+                     * */
+                    origemLeite.setVisibility(View.GONE);
+                    tipoLeite.setVisibility(View.GONE);
+                    selecionarFraude.setVisibility(View.GONE);
+                    contentFrauda.setVisibility(View.GONE);
+
+                    // Desabilita algumas análises
+                    analisesCelulasSomaticas.setVisibility(View.GONE);
+                    analisesCaseina.setVisibility(View.GONE);
+                    analisesNitrogenioUreico.setVisibility(View.GONE);
+                    analisePontoCrioscopico.setVisibility(View.GONE);
+                    analiseAcidezDornic.setVisibility(View.GONE);
+                    analisesResiduoAntibiotico.setVisibility(View.GONE);
+                    analiseDensidadeRelativa.setVisibility(View.GONE);
+                    analisesComposicao.setVisibility(View.GONE);
+                    analiseContagemBacteriaTotal.setVisibility(View.GONE);
+                    analisesCondutividadeEletrica.setVisibility(View.GONE);
+
+                    // Habilita
+                    analisesPh.setVisibility(View.VISIBLE);
+                    analiseContagemBacteriaTotal.setVisibility(View.VISIBLE);
+
+                }
+                else{
+                    /* Setar como Visível os spinners de origem e tipo de leite -> no caso quando
+                     *  o produto selecionado não for o leite.
+                     * */
+                    origemLeite.setVisibility(View.VISIBLE);
+                    tipoLeite.setVisibility(View.VISIBLE);
+                    selecionarFraude.setVisibility(View.VISIBLE);
+                    contentFrauda.setVisibility(View.VISIBLE);
+
+                    // tudo habilitado
+                    analisesCelulasSomaticas.setVisibility(View.VISIBLE);
+                    analisesCaseina.setVisibility(View.VISIBLE);
+                    analisesNitrogenioUreico.setVisibility(View.VISIBLE);
+                    analisePontoCrioscopico.setVisibility(View.VISIBLE);
+                    analiseAcidezDornic.setVisibility(View.VISIBLE);
+                    analisesResiduoAntibiotico.setVisibility(View.VISIBLE);
+                    analiseDensidadeRelativa.setVisibility(View.VISIBLE);
+                    analisesComposicao.setVisibility(View.VISIBLE);
+                    analiseContagemBacteriaTotal.setVisibility(View.VISIBLE);
+
+                    // Habilita
+                    analisesPh.setVisibility(View.VISIBLE);
+                    analiseContagemBacteriaTotal.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        // Implementação para o spinner de Tipo do Leite
+        this.tipoLeite.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("ItemSelected", "Position: " + position + " id: " + id);
+
+                if(position == 0){
+                    // Habilita as opções de seleção de tipos de análises de fraude
+                    selecionarFraude.setVisibility(View.VISIBLE);
+                    contentFrauda.setVisibility(View.VISIBLE);
+
+                    // todas as opções são habilitadas
+                    analisesCelulasSomaticas.setVisibility(View.VISIBLE);
+                    analisesCaseina.setVisibility(View.VISIBLE);
+                    analisesNitrogenioUreico.setVisibility(View.VISIBLE);
+                    analisePontoCrioscopico.setVisibility(View.VISIBLE);
+                    analiseAcidezDornic.setVisibility(View.VISIBLE);
+                    analisesResiduoAntibiotico.setVisibility(View.VISIBLE);
+                    analiseContagemBacteriaTotal.setVisibility(View.VISIBLE);
+
+                }
+                else if(position == 1){
+                    // Desabilita as opções de seleção de tipos de análises de fraude
+                    selecionarFraude.setVisibility(View.GONE);
+                    contentFrauda.setVisibility(View.GONE);
+
+                    // Desabilita algumas opções de análises
+                    analisesCelulasSomaticas.setVisibility(View.GONE);
+                    analisesCaseina.setVisibility(View.GONE);
+                    analisesNitrogenioUreico.setVisibility(View.GONE);
+                    analisePontoCrioscopico.setVisibility(View.GONE);
+                    analiseAcidezDornic.setVisibility(View.GONE);
+                    analisesResiduoAntibiotico.setVisibility(View.GONE);
+
+                    // Habilita
+                    analiseContagemBacteriaTotal.setVisibility(View.VISIBLE);
+                }
+                else{
+                    // Desabilita as opções de seleção de tipos de análises de fraude
+                    selecionarFraude.setVisibility(View.GONE);
+                    contentFrauda.setVisibility(View.GONE);
+
+                    // Desabilita algumas opções de análises
+                    analisesCelulasSomaticas.setVisibility(View.GONE);
+                    analisesCaseina.setVisibility(View.GONE);
+                    analisesNitrogenioUreico.setVisibility(View.GONE);
+                    analisePontoCrioscopico.setVisibility(View.GONE);
+                    analiseAcidezDornic.setVisibility(View.GONE);
+                    analisesResiduoAntibiotico.setVisibility(View.GONE);
+                    analiseContagemBacteriaTotal.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+    }
+
+
+    // Ajusta o layout inicial
+    public void iniciaLayoutAnalise(){
+        /* Setar como invisível os spinners de origem e tipo de leite -> no caso quando
+         *  o produto selecionado não for o leite.
+         * */
+        origemLeite.setVisibility(View.GONE);
+        tipoLeite.setVisibility(View.GONE);
+        selecionarFraude.setVisibility(View.GONE);
+        contentFrauda.setVisibility(View.GONE);
+
+        // Desabilita algumas análises
+        analisesCelulasSomaticas.setVisibility(View.GONE);
+        analisesCaseina.setVisibility(View.GONE);
+        analisesNitrogenioUreico.setVisibility(View.GONE);
+        analisePontoCrioscopico.setVisibility(View.GONE);
+        analiseAcidezDornic.setVisibility(View.GONE);
+        analisesResiduoAntibiotico.setVisibility(View.GONE);
+        analiseDensidadeRelativa.setVisibility(View.GONE);
+
+        // Habilita algumas análises
+        analisesComposicao.setVisibility(View.VISIBLE);
+        analisesPh.setVisibility(View.VISIBLE);
+        analisesCondutividadeEletrica.setVisibility(View.VISIBLE);
+        analiseContagemBacteriaTotal.setVisibility(View.VISIBLE);
     }
 
     /* salva as informaçoes selecionadas e manda para a activity ListarAnalises ...
